14027557a00a25c35f8fae54c389cb31395b657e
Stanley Varner <stanmanjr28@gmail.com>
2018-06-14 14:21:53 -0600

    js folder returned to original

--- assignments/lesson-7/scripts/windchill.js
+++ assignments/lesson-7/scripts/windchill.js
@@ -5,3 +5,5 @@ var t = (HighTemp + LowTemp) / 2;
 var s_pow = Math.pow(s,0.16);
 var WindChill = 35.74 + (0.6215 * t) - (35.75 * s_pow) + (0.4275 * t * s_pow);
 document.getElementById("wind-chill").innerHTML = Math.round(WindChill);
+
+//fixed
