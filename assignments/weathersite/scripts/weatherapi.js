d17d545a4523d2504062df0bc5c5f2bf89660999
Stanley Varner <stanmanjr28@gmail.com>
2018-07-01 18:32:28 -0600

    weather site

---
+++ assignments/weathersite/scripts/weatherapi.js
@@ -0,0 +1,14 @@
+var weatherRequest = new XMLHttpRequest();
+var apiURL = "https://api.openweathermap.org/data/2.5/weather?id=4156210&units=imperial&APPID=6bc4c6f662e6edbf7ba9fb1a75be0863"
+weatherRequest.open('GET', apiURL, true);
+weatherRequest.send();
+weatherRequest.onload =  function () {
+    let weatherData = JSON.parse(weatherRequest.responseText);
+    console.log(weatherData);
+
+    document.getElementById("current-temp").innerHTML = weatherData.main.temp;
+
+    let imagesrc = 'https://openweathermap.org/img/w/' + weatherData.weather[0].icon + '.png';
+    document.getElementById("icon-weather").src = imagesrc;
+
+}
