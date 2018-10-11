8341f7921c12404b6cd42f696a653253600e5493
Stanley Varner <stanmanjr28@gmail.com>
2018-10-11 12:14:15 -0600

    parse2

--- cit261/parse.js
+++ cit261/parse.js
@@ -2,14 +2,3 @@ var txt = '{"name":"John", "age":30, "city":"New York", "food":"Pizza", "transpo
 
 var obj = JSON.parse(txt);
 document.getElementById("demo").innerHTML = obj.name + ", " + obj.age + ", " + obj.food + ", " + obj.transporation;
-
-
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var myObj = JSON.parse(this.responseText);
-        document.getElementById("demo2").innerHTML = myObj.name;
-    }
-};
-xmlhttp.open("GET", "json_demo.txt", true);
-xmlhttp.send();
